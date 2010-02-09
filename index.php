aeb287ce3a4f3fa596848aff3d6684bc581400d8
Mathieu Virbel <tito@bankiz.org>
2010-02-09 13:51:22 +0100

    add piwik tag

--- index.php
+++ index.php
@@ -43,5 +43,17 @@
 			</p>
 		</div>
 	</div>
+
+	<script type="text/javascript">
+	var pkBaseURL = (("https:" == document.location.protocol) ? "https://txzone.net/piwik/" : "http://txzone.net/piwik/");
+	document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+	</script><script type="text/javascript">
+		try {
+			var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 4);
+			piwikTracker.trackPageView();
+			piwikTracker.enableLinkTracking();
+		} catch( err ) {}
+	</script><noscript><p><img src="http://txzone.net/piwik/piwik.php?idsite=4" style="border:0" alt=""/></p></noscript>
+
 </body>
 </html>
